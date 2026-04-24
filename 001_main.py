24ea3fc8b42f5ebd4f4606f857bc9a56242e2779
srshivu <shivananddevakate497@gmail.com>
2026-04-24 09:26:26 +0530

    new

--- 001_main.py
+++ 001_main.py
@@ -1,5 +1,20 @@
-import fastapi import FastAPI
-app=FastAPI()
-@app.get("/")
-def home():
-    return "hello shiva"
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+students = {
+    1: {"name": "shiva", "age": 21, "marks": 71},
+    2: {"name": "kehoo", "age": 22, "marks": 22}
+}
+
+@app.get("/student/{student_id}")
+def get_student(student_id: int, field: str = None):
+    if student_id not in students:
+        raise HTTPException(status_code=404, detail="Student not found")
+
+    if field:
+        if field not in students[student_id]:
+            raise HTTPException(status_code=404, detail="Field not found")
+        return {field: students[student_id][field]}
+
+    return students[student_id]
--- bubble.py
+++ bubble.py
@@ -1,6 +1,8 @@
-li=[23,94,87,97,88]
-for i in range(len(li)):
-    for j in range(1,len(li)-i):
-         if li[j] > li[j-1]:
-              li[j],li[j-1]=li[j-1],li[j]
-print((li))              
+students = {
+    1: {
+        "name": "shiva",
+        "age": 21,
+        "marks": 71
+    }
+}
+print(students[1])
